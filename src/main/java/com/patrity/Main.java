1eb022b0e4525885e537066221f2b02c98d4273a
Tony Costanzo <tonycos92@gmail.com>
2021-09-15 10:05:08 -0500

    Thread safe api calls

--- src/main/java/com/patrity/Main.java
+++ src/main/java/com/patrity/Main.java
@@ -10,30 +10,53 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     private static Main SINGLETON;
     private static NobilityData nobilityData;
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public static void main (String[] args) throws IOException, InterruptedException {
         Main.SINGLETON = new Main();
+
+
+        nobilityData = SINGLETON.updateSupply();
+
+        Main.SINGLETON.executorService.submit(() -> {
+            try {
+                Main.SINGLETON.init();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
         Javalin app = Javalin.create().start(6969);
         app.get("/", Main::serveSupply);
-        Main.SINGLETON.updateSupply();
     }
 
-    private void updateSupply() throws IOException, InterruptedException {
+    private void init() throws InterruptedException, IOException {
         while (true) {
-            String supply = Data.getSupply();
-            String holders = Data.getHolders();
-            DexToolsV1 dexToolsV1 = Data.getV1Data();
-            DexToolsV2 dexToolsV2 = Data.getV2Data();
-            LBank lBank = Data.getLBankData();
-
-            nobilityData = new NobilityData(supply, holders, dexToolsV1.getPriceChange24h(), dexToolsV1.getVolume24hUSD(), dexToolsV2.getToken_price_usd(), lBank);
+            nobilityData = updateSupply();
             Thread.sleep(30000L);
         }
+    }
+
+    private NobilityData updateSupply() throws IOException, InterruptedException {
+        String supply = Data.getSupply();
+        String holders = Data.getHolders();
+        DexToolsV1 dexToolsV1 = Data.getV1Data();
+        DexToolsV2 dexToolsV2 = Data.getV2Data();
+        LBank lBank = Data.getLBankData();
+        Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("Supply updated: " + timeStamp);
+        return new NobilityData(supply, holders, dexToolsV1.getPriceChange24h(), dexToolsV1.getVolume24hUSD(), dexToolsV2.getToken_price_usd(), lBank);
     }
 
     private static void serveSupply(Context ctx) {
